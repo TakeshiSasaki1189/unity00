cb1a360892d4cba8a1e0d7ae1f5cef2209d075f5
TakeshiSasaki1189 <sasaki@mossjp.co.jp>
2017-05-24 18:23:06 +0900

    MultiScene Editing

--- unity00/Assets/Script/Manager.cs
+++ unity00/Assets/Script/Manager.cs
@@ -5,9 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class Manager : MonoBehaviour {
 
+	int	LoadFlag;
+	string	ButtonStr;
+
 	// Use this for initialization
 	void Start () {
 
+		LoadFlag = 0;
+		ButtonStr = "Load";
 		SceneManager.LoadScene("Scene_1",LoadSceneMode.Additive );
 	}
 	
@@ -18,15 +23,33 @@ public class Manager : MonoBehaviour {
 
 	void OnGUI()
 	{
-		if (GUI.Button (new Rect (0,0,Screen.width / 6,Screen.height /6), "Scene_1->Scene_2")) {
-			
+//		if (GUI.Button (new Rect (0,0,Screen.width / 6,Screen.height /6), "Active")) {
+		if (GUI.Button (new Rect (0,0,Screen.width / 6,Screen.height /6), ButtonStr)) {
+
 			StartCoroutine(SceneMove());
+
 		}
 	}
 
 
 	IEnumerator SceneMove()
 	{
+		LoadFlag ^= 1;
+
+		if ( LoadFlag == 1 ){
+			SceneManager.LoadScene("Scene_2",LoadSceneMode.Additive );
+			ButtonStr = "Unload";
+		}else{
+			SceneManager.UnloadSceneAsync("Scene_2");
+			ButtonStr = "Load";
+		}
+
+		//１フレーム待つ、コルーチン必！
+		yield return new WaitForSeconds(Time.deltaTime);
+
+
+
+/**
 		//loadシーンアクティブ
 //		SceneManager.LoadScene("load",LoadSceneMode.Additive );
 		
@@ -34,7 +57,7 @@ public class Manager : MonoBehaviour {
 		yield return new WaitForSeconds(Time.deltaTime);
 		
 		//アニメーション長さ分待つ&アニメーション再生
-//		yield return new WaitForSeconds(load.Instance.Gate_in());
+		yield return new WaitForSeconds(load.Instance.Gate_in());
 		
 		//次のシーンアクティブ
 		SceneManager.LoadScene("Scene_2",LoadSceneMode.Additive );
@@ -43,16 +66,18 @@ public class Manager : MonoBehaviour {
 		yield return new WaitForSeconds(Time.deltaTime);
 		
 		//前のシーン非アクティブ
-//		SceneManager.UnloadScene ("Scene_1");
+		SceneManager.UnloadScene ("Scene_1");
 		
 		//ロード時間とりあえず今回は時間で判定
 		yield return new WaitForSeconds(3);
 		
 		//アニメーション長さ分待つ&アニメーション再生
-//		yield return new WaitForSeconds(load.Instance.Gate_out());
+		yield return new WaitForSeconds(load.Instance.Gate_out());
 		
 		//ロードシーン非アクティブ
-//		SceneManager.UnloadScene ("load");
+		SceneManager.UnloadScene ("load");
+**/
+
 	}
 
 
